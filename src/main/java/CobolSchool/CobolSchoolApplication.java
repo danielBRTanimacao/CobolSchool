1e4b71e5f6a045a7c2508d7119d20df0a8b8a5de
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-07 15:27:42 -0300

    building project

---
+++ src/main/java/CobolSchool/CobolSchoolApplication.java
@@ -0,0 +1,13 @@
+package CobolSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobolSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobolSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CobolSchool/CobolSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package CobolSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobolSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
