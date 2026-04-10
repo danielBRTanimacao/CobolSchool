c6f7bc4a567570725f7e1c1bb05dc6ad52a0bf39
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-10 08:57:44 -0300

    feat: creting basic entity

---
+++ src/main/java/CobolSchool/entities/CourseEntity.java
@@ -0,0 +1,19 @@
+package CobolSchool.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+}
