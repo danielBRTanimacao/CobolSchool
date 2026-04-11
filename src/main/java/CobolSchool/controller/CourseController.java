ecb86b684c832fa2741488148115db06b4c48356
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-11 20:00:51 -0300

    feat: updating imports

--- src/main/java/CobolSchool/controller/CourseController.java
+++ src/main/java/CobolSchool/controller/CourseController.java
@@ -9,9 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
-import java.util.List;
-
 @RestController
 @RequestMapping("api/courses")
 @RequiredArgsConstructor
