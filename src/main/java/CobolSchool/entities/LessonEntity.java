526788aef70d6984160d1448d87266dc9777f28d
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-12 15:46:53 -0300

    feat creating lesson entity

---
+++ src/main/java/CobolSchool/entities/LessonEntity.java
@@ -0,0 +1,20 @@
+package CobolSchool.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class LessonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+}
