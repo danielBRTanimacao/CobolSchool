a36654f88129e9b20030af3a60d17ad29bf9808e
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-28 10:28:32 -0300

    refact: creating a function to re use storage file

--- src/main/java/CobolSchool/service/CourseService.java
+++ src/main/java/CobolSchool/service/CourseService.java
@@ -5,6 +5,7 @@ import CobolSchool.DTOs.courses.RequestUpdateCourseDTO;
 import CobolSchool.entities.CourseEntity;
 import CobolSchool.repository.CourseRepository;
 import CobolSchool.repository.LessonRepository;
+import CobolSchool.utils.StorageProcess;
 import CobolSchool.utils.customs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,14 +30,13 @@ public class CourseService {
     private final CourseRepository repository;
     private final LessonRepository lessonRepository;
 
-    @Value("${upload.path}")
-    private String uploadPath;
+    private final StorageProcess strProcess;
 
     public void saveCourse(RequestCourseDTO data) {
         CourseEntity course = new CourseEntity();
         course.setTitle(data.title());
 
-        String fileName = storeFile(data.image());
+        String fileName = strProcess.storeFile(data.image());
         course.setThumbnailPath(fileName);
 
         repository.save(course);
@@ -62,27 +62,5 @@ public class CourseService {
     public Page<CourseEntity> getAllCourses(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return repository.findAll(pageable);
-    }
-
-    private String storeFile(MultipartFile file) {
-        try {
-            Path rootPath = Paths.get(uploadPath).toAbsolutePath().normalize();
-
-            if (!Files.exists(rootPath)) {
-                Files.createDirectories(rootPath);
-            }
-
-            String originalFileName = file.getOriginalFilename() != null ? file.getOriginalFilename() : "file";
-            String cleanFileName = originalFileName.replaceAll("[^a-zA-Z0-9.\\-]", "_");
-            String fileName = UUID.randomUUID() + "_" + cleanFileName;
-
-            Path targetLocation = rootPath.resolve(fileName);
-
-            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-
-            return fileName;
-        } catch (IOException ex) {
-            throw new RuntimeException("Err saving file: " + ex.getMessage());
-        }
     }
 }
--- src/main/java/CobolSchool/utils/StorageProcess.java
+++ src/main/java/CobolSchool/utils/StorageProcess.java
@@ -1,4 +1,41 @@
 package CobolSchool.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Component
 public class StorageProcess {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    public String storeFile(MultipartFile file) {
+        try {
+            Path rootPath = Paths.get(uploadPath).toAbsolutePath().normalize();
+
+            if (!Files.exists(rootPath)) {
+                Files.createDirectories(rootPath);
+            }
+
+            String originalFileName = file.getOriginalFilename() != null ? file.getOriginalFilename() : "file";
+            String cleanFileName = originalFileName.replaceAll("[^a-zA-Z0-9.\\-]", "_");
+            String fileName = UUID.randomUUID() + "_" + cleanFileName;
+
+            Path targetLocation = rootPath.resolve(fileName);
+
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (IOException ex) {
+            throw new RuntimeException("Err saving file: " + ex.getMessage());
+        }
+    }
 }
