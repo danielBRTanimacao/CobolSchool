d3c23f1af66d7a277a0f4d6cb57d72484ae84da2
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-04-30 10:47:42 -0300

    feat changing multipart file

--- src/main/java/CobolSchool/DTOs/lessons/RequestLessonDTO.java
+++ src/main/java/CobolSchool/DTOs/lessons/RequestLessonDTO.java
@@ -1,12 +1,14 @@
 package CobolSchool.DTOs.lessons;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public record RequestLessonDTO(
         @NotBlank
         String name,
         MultipartFile image,
-        String video
+        @NotNull
+        MultipartFile video
 ) {
 }
