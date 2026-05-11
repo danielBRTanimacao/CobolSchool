3e0a6da01b5ffd828feac5848778c52b279b8cf1
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-05-11 16:57:57 -0300

    feat collect data

--- src/main/java/CobolSchool/config/CorsConfig.java
+++ src/main/java/CobolSchool/config/CorsConfig.java
@@ -4,6 +4,7 @@ package CobolSchool.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:upload/");
     }
 }
