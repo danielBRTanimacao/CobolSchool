5e56b0b6bbb506d2c05576fe0c1a8f7321b76b12
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-05-19 08:10:02 -0300

    feat testing and updating repository

--- src/main/java/CobolSchool/repository/UserRepository.java
+++ src/main/java/CobolSchool/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package CobolSchool.repository;
 
-public class UserRepository {
+import CobolSchool.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
 }
---
+++ src/main/java/CobolSchool/service/TokenService.java
@@ -0,0 +1,50 @@
+package CobolSchool.service;
+
+import CobolSchool.entities.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static org.springframework.security.config.Elements.JWT;
+
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserEntity user) {
+        try {
+            log.debug("Generating token for user: {}", user.getEmail());
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException e) {
+            log.error("Error while generating token for user: {}", user.getEmail(), e);
+            throw new TokenException("Error while authenticating");
+        }
+    }
+
+    public String validateTokens(String token){
+        try {
+            log.debug("Validating token: {}", token);
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            log.error("Error while validating token: {}", e.getMessage());
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(23).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
