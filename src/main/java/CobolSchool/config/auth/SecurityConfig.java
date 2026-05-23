efa5ba1868b0c67ad48b611714cb0547246dcd79
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-05-23 09:40:18 -0300

    feat impl new filter

--- src/main/java/CobolSchool/config/auth/SecurityConfig.java
+++ src/main/java/CobolSchool/config/auth/SecurityConfig.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
