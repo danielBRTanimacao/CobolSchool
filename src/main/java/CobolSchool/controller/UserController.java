e16e1b89eb74577e5f17588f85f43bfb4a7797f8
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-05-28 09:08:34 -0300

    service and user controll

--- src/main/java/CobolSchool/controller/UserController.java
+++ src/main/java/CobolSchool/controller/UserController.java
@@ -1,9 +1,12 @@
 package CobolSchool.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
 }
