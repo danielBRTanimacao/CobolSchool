90ebf875e03e840783aaf38b568510eea6e366dd
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-05-29 08:51:04 -0300

    feat updating service for user

--- src/main/java/CobolSchool/service/userService.java
+++ src/main/java/CobolSchool/service/userService.java
@@ -4,4 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class userService {
+    public void login() {
+
+    }
+
+    public void createUser() {
+
+    }
+
+    public void updateUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
 }
