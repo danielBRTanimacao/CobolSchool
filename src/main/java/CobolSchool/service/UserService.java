4256015d80e9af4c0c50fffd2de9d4b20347a3fc
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-01 20:00:29 -0300

    feat created user auth

--- src/main/java/CobolSchool/service/UserService.java
+++ src/main/java/CobolSchool/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     public ResponseUserDTO login() {
-
+        return new ResponseUserDTO("123");
     }
 
     public void createUser() {
