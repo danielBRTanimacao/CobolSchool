2ce3f0bda2aeced08f93568354bb014021bf781a
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-02 16:21:55 -0300

    feat add logs to service

--- src/main/java/CobolSchool/service/CourseService.java
+++ src/main/java/CobolSchool/service/CourseService.java
@@ -1,13 +1,13 @@
 package CobolSchool.service;
 
-import CobolSchool.DTOs.courses.RequestCourseDTO;
-import CobolSchool.DTOs.courses.RequestUpdateCourseDTO;
+import CobolSchool.DTOs.courses.*;
 import CobolSchool.entities.CourseEntity;
 import CobolSchool.repository.CourseRepository;
 import CobolSchool.repository.LessonRepository;
 import CobolSchool.utils.StorageProcess;
 import CobolSchool.utils.customs.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CourseService {
 
     private final CourseRepository repository;
@@ -24,9 +25,11 @@ public class CourseService {
     private final StorageProcess strProcess;
 
     public void saveCourse(RequestCourseDTO data) {
+        log.debug("Creating course entity");
         CourseEntity course = new CourseEntity();
         course.setTitle(data.title());
 
+        log.debug("generating file save root path");
         String fileName = strProcess.storeFile(data.image());
         course.setThumbnailPath(fileName);
 
@@ -34,12 +37,14 @@ public class CourseService {
     }
 
     public CourseEntity getCourse(Long id) {
+        log.debug("find specific course by Id");
         return repository.findById(id).orElseThrow(
                 () -> new NotFoundException("Course not found")
         );
     }
 
     public void deleteCourse(Long id) {
+        log.debug("Deleting course by Id");
         CourseEntity course = repository.findById(id).orElseThrow(
                 () -> new NotFoundException("Course not found")
         );
@@ -47,10 +52,12 @@ public class CourseService {
     }
 
     public void updateCourse(RequestUpdateCourseDTO data, Long id) {
+        log.debug("Find course by id");
         CourseEntity course = repository.findById(id).orElseThrow(
                 () -> new NotFoundException("Course not found")
         );
 
+        log.debug("Find not null data to set new characters");
         if (data.title() != null) {
             course.setTitle(data.title());
         }
