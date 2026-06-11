477cc8feb98f6d23b13dd8dd435569e4e660ac00
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-11 11:15:56 -0300

    feat creating email submission

--- src/main/java/CobolSchool/utils/EmailService.java
+++ src/main/java/CobolSchool/utils/EmailService.java
@@ -1,7 +1,25 @@
 package CobolSchool.utils;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-@Component
-public class EmailService {
+import java.util.Properties;
+
+@Bean
+public JavaMailSender EmailService() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
+
+    mailSender.setUsername("my.gmail@gmail.com");
+    mailSender.setPassword("password");
+
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
+    return mailSender;
 }
